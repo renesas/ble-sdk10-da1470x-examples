6d9566c41d32ae49bfccd7de7b7b860fc222b014
Evangelos Kalkounis <evangelos.kalkounis@diasemi.com>
2022-02-08 19:57:43 +0200

    Register the BLE custom service, deregister all other services

--- connectivity/ble_custom_service/config/ble_peripheral_config.h
+++ connectivity/ble_custom_service/config/ble_peripheral_config.h
@@ -5,7 +5,7 @@
  *
  * @brief Application configuration
  *
- * Copyright (C) 2015-2020 Dialog Semiconductor.
+ * Copyright (C) 2015-2022 Dialog Semiconductor.
  * This computer program includes Confidential, Proprietary Information
  * of Dialog Semiconductor. All Rights Reserved.
  *
@@ -16,14 +16,16 @@
 #define BLE_PERIPHERAL_CONFIG_H_
 
 // enable debug service (see readme.txt for details)
-#define CFG_DEBUG_SERVICE    (1)
+#define CFG_DEBUG_SERVICE    (0)
 
-#define CFG_BAS              (1)     // register BAS service
-#define CFG_BAS_MULTIPLE     (1)     // add 2 instances of BAS service
-#define CFG_CTS              (1)     // register CTS
-#define CFG_DIS              (1)     // register DIS
-#define CFG_DIS_FULL         (1)     // add all possible characteristics to DIS
-#define CFG_SCPS             (1)     // register ScPS
-#define CFG_USER_SERVICE     (1)     // register custom service (using 128-bit UUIDs)
+#define CFG_BAS              (0)     // register BAS service
+#define CFG_BAS_MULTIPLE     (0)     // add 2 instances of BAS service
+#define CFG_CTS              (0)     // register CTS
+#define CFG_DIS              (0)     // register DIS
+#define CFG_DIS_FULL         (0)     // add all possible characteristics to DIS
+#define CFG_SCPS             (0)     // register ScPS
+#define CFG_USER_SERVICE     (0)     // register custom service (using 128-bit UUIDs)
+
+#define CFG_MY_CUSTOM_SERVICE   (1)     // my custom service
 
 #endif /* BLE_PERIPHERAL_CONFIG_H_ */
