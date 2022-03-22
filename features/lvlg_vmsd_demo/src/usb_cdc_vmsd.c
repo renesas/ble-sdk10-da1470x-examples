99a11eb8ae85cd0bb7c99b5bfba4926adf57ed87
Aart Kleinendorst <aart.kleinendorst@diasemi.com>
2022-03-22 15:53:44 +0100

    added latest USB library (from Development branch)

--- features/lvlg_vmsd_demo/src/usb_cdc_vmsd.c
+++ features/lvlg_vmsd_demo/src/usb_cdc_vmsd.c
@@ -19,9 +19,11 @@
 #include "USB_CDC.h"
 #include "USB_MSD.h"
 #include "USB_VirtualMSD.h"
+#include "sys_tcs.h"
 #include "readme_html.h"
-
-#if (DEVICE_FAMILY == DA1469X) || (DEVICE_FAMILY == DA1470X)
+#if (DEVICE_FAMILY == DA1468X)
+#include "sys_clock_mgr.h"
+#elif (DEVICE_FAMILY == DA1469X) || (DEVICE_FAMILY == DA1470X)
 #if ( dg_configUSE_SYS_CHARGER == 1 )
 #include "custom_charging_profile.h"
 #endif
@@ -33,10 +35,10 @@
  *
  **********************************************************************
  */
-#define VMSD_USE_NVMS //Use NVMS or RAM for data
-#define VMSD_DATA_SIZE 2*1024 //file size
-#define _VIRTUALMSD_NUM_SECTORS (32+512*2) //8kbyte
-#define MAX_CONST_FILE 1
+#define VMSD_USE_NVMS                           //Use NVMS or RAM for data
+#define VMSD_DATA_SIZE 2*1024                   //file size
+#define _VIRTUALMSD_NUM_SECTORS (32+512*2)        //8kbyte
+#define MAX_CONST_FILE 3
 #define APP_FILE_HEADER "FWBIN"
 #define usb_main_TASK_PRIORITY              ( OS_TASK_PRIORITY_NORMAL )
 
@@ -54,7 +56,7 @@ static bool isFwFile;
 static const USB_DEVICE_INFO _DeviceInfo = {
         0x2DCF,         // VendorId
         0x6003,         // ProductId
-        "Dialog Semiconductor",       // VendorName
+        "Renesas Electronics",       // VendorName
 #if (DEVICE_FAMILY == DA1468X)
         "DA1468x/DA1510x VirtualMSD device",  // ProductName
 #elif (DEVICE_FAMILY == DA1469X)
@@ -62,17 +64,17 @@ static const USB_DEVICE_INFO _DeviceInfo = {
 #elif (DEVICE_FAMILY == DA1470X)
         "DA1470x VirtualMSD device",  // ProductName
 #endif
-        "0123456789AB" // SerialNumber. Should be 12 character or more for compliance with Mass Storage Device Bootability spec.
-        };
+        "0123456789AB"                            // SerialNumber. Should be 12 character or more for compliance with Mass Storage Device Bootability spec.
+};
 //
 // String information used when inquiring the volume 0.
 //
 static const USB_MSD_LUN_INFO _Lun0Info = {
-        "Vendor",     // MSD VendorName
-        "MSD Volume", // MSD ProductName
-        "1.00",       // MSD ProductVer
-        "134657890"   // MSD SerialNo
-        };
+        "Vendor",                                // MSD VendorName
+        "MSD Volume",                            // MSD ProductName
+        "1.00",                                  // MSD ProductVer
+        "134657890"                              // MSD SerialNo
+};
 
 //
 // Remaining bytes of sector are filled with 0s on read, if a file does not occupy complete sectors
@@ -193,9 +195,9 @@ static void _memFree(void *p)
 static const USB_VMSD_USER_FUNC_API _UserFuncAPI = {
         _cbOnRead,     // pfOnRead    -> Is called when a sector of a given file is read.
         _cbOnWrite,    // pfOnWrite   -> Is called when a sector of a given file is written.
-        _memAlloc, // pfMemAlloc  -> Optional, can be set in order to allow the VirtualMSD to share the mem alloc function of a system.
-        _memFree // pfMemFree   -> Optional, can be set in order to allow the VirtualMSD to share the mem free function of a system.
-        };
+        _memAlloc,     // pfMemAlloc  -> Optional, can be set in order to allow the VirtualMSD to share the mem alloc function of a system.
+        _memFree       // pfMemFree   -> Optional, can be set in order to allow the VirtualMSD to share the mem free function of a system.
+};
 
 /*********************************************************************
  *
@@ -227,6 +229,21 @@ void USB_VMSD_X_Config(void)
         USBD_VMSD_AddConstFiles(0, &_aConstFiles[0], SEGGER_COUNTOF(_aConstFiles)); // Push const file to the volume
 }
 
+uint32_t USB_VMSD_FS_BootSector_VolID_Config(void)
+{
+        /* Use product info as volume id of the VMSD device */
+        uint32_t *variant = NULL;
+        uint8_t size = 0;
+
+        sys_tcs_get_custom_values(SYS_TCS_GROUP_PROD_INFO, &variant, &size);
+
+        // If the Device Variant entry has been successfully retrieved
+        if ((size == 3) && (variant != NULL)) {
+                return *variant;
+        }
+
+        return 0;
+}
 
 void usb_cdc_vmsd_state_cb(void * pContext, U8 NewState)
 {
@@ -256,7 +273,7 @@ void usb_cdc_vmsd_state_cb(void * pContext, U8 NewState)
  *       Usb_cdc_eco_task
  *
  */
-void usb_cdc_eco_task(void *params)
+OS_TASK_FUNCTION(usb_cdc_eco_task, params)
 {
         USB_CDC_HANDLE hInst;
         int NumBytesReceived;
@@ -302,14 +319,17 @@ void usb_cdc_eco_task(void *params)
         }
 }
 
-void usb_vmsd_task(void *params)
+OS_TASK_FUNCTION(usb_vmsd_task, params)
 {
+        USB_CDC_HANDLE hInst;
+        OS_BASE_TYPE status;
 
         USBD_Init();
-        USBD_CDC_Init();        USBD_VMSD_Init();
-        USBD_RegisterSCHook(&UsbpHook, usb_cdc_vmsd_state_cb, NULL);
         USBD_EnableIAD();
-
+        USBD_CDC_Init();
+        USBD_VMSD_Init();
+        USBD_RegisterSCHook(&UsbpHook, usb_cdc_vmsd_state_cb, NULL);
+        //hInst = _AddCDC();
         USBD_VMSD_Add();
         USBD_SetDeviceInfo(&_DeviceInfo);
 #if ( dg_configUSE_SYS_CHARGER == 1 )
@@ -347,8 +367,8 @@ void usb_cdc_vmsd_start()
                                                    debug only; not used by the kernel. */
                         usb_vmsd_task,          /* The function that implements the task. */
                         NULL,                   /* The parameter passed to the task. */
-                        2048, /* The number of bytes to allocate to the
-                                                     stack of the task. */
+                        1024,                   /* The number of bytes to allocate to the
+                                                                     stack of the task. */
                         usb_main_TASK_PRIORITY, /* The priority assigned to the task. */
                         usb_vmsd_task_handle);  /* The task handle. */
 
@@ -361,8 +381,10 @@ void usb_cdc_vmsd_stop()
         USBD_DeInit();
         OS_TASK_DELETE(usb_cdc_task_handle);
         OS_TASK_DELETE(usb_vmsd_task_handle);
+        run_usb_task = 0;
 }
 
+#if (DEVICE_FAMILY == DA1468X)
 /*********************************************************************
  *
  *       usb_start_enumeration_cb
@@ -374,11 +396,29 @@ void usb_cdc_vmsd_stop()
  *          call-backs may be called before the application task is started.
  *          The application code should handle this case, if need be.
  */
-#if (DEVICE_FAMILY == DA1468X)
 void usb_start_enumeration_cb(void)
+{
+        if (run_usb_task == 0) {
+                _ReattchRequest = 0;
+                pm_sleep_mode_request(pm_mode_active);
+                run_usb_task = 1;
+                isFwFile = false;
+                cm_sys_clk_set(sysclk_PLL96);
+                hw_usb_init();
+                hw_usb_bus_attach();
+                usb_cdc_vmsd_start();
+        }
+}
 #elif (DEVICE_FAMILY == DA1469X) || (DEVICE_FAMILY == DA1470X)
+/*********************************************************************
+ *
+ *       sys_usb_ext_hook_begin_enumeration
+ *
+ *  Function description
+ *    Event callback called from the VBUS event to notify
+ *    the application that need to proceed with enumeration.
+ */
 void sys_usb_ext_hook_begin_enumeration(void)
-#endif
 {
         if (run_usb_task == 0) {
                 _ReattchRequest = 0;
@@ -390,6 +430,9 @@ void sys_usb_ext_hook_begin_enumeration(void)
         }
 
 }
+#endif
+
+#if (DEVICE_FAMILY == DA1468X)
 /*********************************************************************
  *
  *       usb_detach_cb
@@ -402,14 +445,30 @@ void sys_usb_ext_hook_begin_enumeration(void)
  *          call-backs may be called before the application task is started.
  *          The application code should handle this case, if need be.
  */
-#if (DEVICE_FAMILY == DA1468X)
 void usb_detach_cb(void)
+{
+        if (run_usb_task == 1) {
+                hw_usb_bus_detach();
+                usb_cdc_vmsd_stop();
+                run_usb_task = 0;
+                cm_sys_clk_set(dg_configDEFAULT_CLK);
+                pm_sleep_mode_release(pm_mode_active);
+        }
+}
 #elif (DEVICE_FAMILY == DA1469X) || (DEVICE_FAMILY == DA1470X)
+/*********************************************************************
+ *
+ *       sys_usb_ext_hook_detach
+ *
+ *  Function description
+ *    Event callback called from the VBUS event to notify
+ *    the application that the USB cable was removed and need to detach.
+ */
 void  sys_usb_ext_hook_detach(void)
-#endif
 {
         if (run_usb_task == 1) {
                 usb_cdc_vmsd_stop();
                 run_usb_task = 0;
         }
 }
+#endif
